1b9895b3c11f5212c6c27be97082e20e256dbe46
BartekSliwka <bartek67898@gmail.com>
2025-05-20 19:58:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ if selected:
     # Filtrowanie danych dla wybranych filmów, usuwanie NaN i sortowanie malejąco według Gross
     selected_df = (
         df[df["Series_Title"].isin(selected)][["Series_Title", "Released_Year", "Gross"]]
-        .dropna(subset=["Gross"])  # Usuwamy filmy z brakującymi wartościami Gross
+        .dropna(subset=["Gross"])
         .sort_values(by="Gross", ascending=False)
     )
 
@@ -30,21 +30,30 @@ if selected:
             selected_df,
             x="Series_Title",
             y="Gross",
-            color_discrete_sequence=["red"],  # Kolor słupków
+            color_discrete_sequence=["red"],
             title="Zysk wybranych filmów",
             labels={"Series_Title": "Tytuł filmu", "Gross": "Zysk (w USD)"},
-            hover_data={"Series_Title": True, "Released_Year": True, "Gross": ":,.2f"},  # Dane w tooltip
         )
 
-        # Dostosowanie osi Y do formatu w milionach
+        # Dostosowanie tooltipów z wytłuszczeniem kategorii
+        fig.update_traces(
+            hovertemplate=(
+                "<b>Tytuł filmu:</b> %{x}<br>" +
+                "<b>Zysk (w USD):</b> %{y:,.2f}<br>" +
+                "<b>Rok wydania:</b> %{customdata}<extra></extra>"
+            ),
+            customdata=selected_df["Released_Year"]  # Przekazanie danych Released_Year do tooltipa
+        )
+
+        # Dostosowanie osi i stylu
         fig.update_layout(
-            yaxis_tickformat=".2fM",  # Format w milionach
+            yaxis_tickformat=".2fM",
             yaxis_title="Zysk (w USD)",
             xaxis_title="Tytuł filmu",
             title_font_size=14,
-            xaxis_tickangle=45,  # Obrót etykiet osi X
-            showlegend=False,  # Ukrycie legendy
-            margin=dict(r=50),  # Margines po prawej
+            xaxis_tickangle=45,
+            showlegend=False,
+            margin=dict(r=50),
         )
 
         # Wyświetlenie wykresu w Streamlit
