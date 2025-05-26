2c8911fcdb4051124ca57e22299e90375f80e942
jomaha213 <1a.kacper.pochopien@gmail.com>
2025-05-27 00:11:34 +0200

    connectdb_for_streamlite

--- connect_db.py
+++ connect_db.py
@@ -1,18 +1,18 @@
 import psycopg2
 from dotenv import load_dotenv
 import os
-
+import streamlit as st
 # Load environment variables from .env
-load_dotenv()
+
 
 def connect_db():
     try:
         conn = psycopg2.connect(
-            host=os.getenv("DB_HOST"),
-            database=os.getenv("DB_NAME"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            port=os.getenv("DB_PORT")
+            dbname=st.secrets["DB_NAME"],
+            user=st.secrets["DB_USER"],
+            password=st.secrets["DB_PASSWORD"],
+            host=st.secrets["DB_HOST"],
+            port=st.secrets["DB_PORT"]
         )
         return conn
     except Exception as e:
