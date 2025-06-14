17794050d59defbe24e77a9929de2a7cbd48578c
jomaha213 <1a.kacper.pochopien@gmail.com>
2025-06-14 20:07:27 +0200

    backup sign.py

--- sign.py
+++ sign.py
@@ -6,11 +6,30 @@ def sign():
         st.session_state.logged_in = False
         st.session_state.username = ""
 
+    manager = LoginManager()
 
     if not st.session_state.logged_in:
-        if not st.session_state.logged_in:
-        manager = LoginManager()
-        manager.login_panel()
+        st.title("Logowanie")
+        option = st.radio("Wybierz opcję", ["Zaloguj się", "Zarejestruj się"])
+
+        username = st.text_input("Login")
+        password = st.text_input("Hasło", type="password")
+
+        if option == "Zaloguj się":
+            if st.button("Zaloguj"):
+                if manager.verify_user(username, password):
+                    st.session_state.logged_in = True
+                    st.session_state.username = username
+                    st.success(f"Zalogowano jako {username}")
+                    st.rerun()
+                else:
+                    st.error("Niepoprawny login lub hasło")
+        else:
+            if st.button("Zarejestruj"):
+                if username and password:
+                    manager.register_user(username, password)
+                else:
+                    st.warning("Wypełnij oba pola!")
             
 
     
